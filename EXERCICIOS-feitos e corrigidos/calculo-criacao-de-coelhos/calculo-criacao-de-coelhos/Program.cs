65d548312f22167e079e89946991ee3ebd2f2536
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-07-14 22:01:23 -0300

    Ex: calculo-criacao-de-coelhos

---
+++ EXERCICIOS-feitos e corrigidos/calculo-criacao-de-coelhos/calculo-criacao-de-coelhos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace calculo_criacao_de_coelhos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double custo;
+            int nCoelhos;
+
+            Console.WriteLine("Informe o número de coelhos: ");
+            nCoelhos = Int32.Parse(Console.ReadLine());
+
+            custo = (nCoelhos * 20) / 28;
+
+            Console.WriteLine($"{custo}");
+
+        }
+    }
+}
